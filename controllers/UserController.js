39406ed30fcd5467b1de85e8f7f873f0fc6ed2cb
VictorSFR <victorsfranca3@outlook.com>
2023-08-23 07:31:31 -0300

    Validação do formulário.

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -60,7 +60,14 @@ class UserController {
     getValues() {
 
         let user = {};
+        let isValid = true;
         [...this.formEl.elements].forEach(function (field, index) {
+
+            if(['name', 'email', 'password'].indexOf(field.name) > -1 && !field.value) {
+                field.parentElement.classList.add('has-error');
+                isValid = false;
+            }
+
             if (field.name == "gender") {
                 if (field.checked) {
                     user[field.name] = field.value;
@@ -74,6 +81,9 @@ class UserController {
 
         });
 
+        if(!isValid){
+            return false;
+        }
         return new User(
             user.name,
             user.gender,
