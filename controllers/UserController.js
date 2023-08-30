5b27ca7a909550651e9b9e85bf2a9ed5e51c23d4
VictorSFR <victorsfranca3@outlook.com>
2023-08-30 10:27:12 -0300

    Programando exclusão de usuário da tabela.

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -115,7 +115,7 @@ class UserController {
             };
             fileReader.onerror = () => {
                 reject(e);
-            };
+            };  
 
             if (file) {
                 fileReader.readAsDataURL(file);
@@ -178,7 +178,7 @@ class UserController {
             <td>${Utils.dateFormat(dataUser.register)}</td>
             <td>
                 <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                <button type="button" class="btn btn-danger btn-delete btn-xs btn-flat">Excluir</button>
             </td>
         `;
 
@@ -190,6 +190,14 @@ class UserController {
     }
 
     addEventsTr(tr) {
+
+        tr.querySelector(".btn-delete").addEventListener("click", e => {
+            if(confirm("Deseja realmente excluir?")){
+                tr.remove();
+                this.updateCount();
+            }
+        });
+
         tr.querySelector(".btn-edit").addEventListener("click", e => {
             let json = JSON.parse(tr.dataset.user);
 
