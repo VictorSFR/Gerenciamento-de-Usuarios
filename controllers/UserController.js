6ae3780f7bed89fe1204cf003972e9bd2b7d3a27
VictorSFR <victorsfranca3@outlook.com>
2023-09-04 15:20:23 -0300

    Finalizando projeto e funções CRUD.

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -3,7 +3,6 @@ class UserController {
         this.formEl = document.getElementById(formIdCreate);
         this.formUpdateEl = document.getElementById(formIdUpdate);
         this.tableEl = document.getElementById(tableId);
-        console.log("tableId = " + tableId);
         this.onSubmit();
         this.onEdit();
         this.selectAll();
@@ -41,23 +40,17 @@ class UserController {
                         result._photo = content;
                     }
 
-                    tr.dataset.user = JSON.stringify(result);
+                    let user = new User();
 
-                    tr.innerHTML = `
-                    <td><img src="${result._photo}" alt="User Image" class="img-circle img-sm"></td>
-                    <td>${result._name}</td>
-                    <td>${result._email}</td>    
-                    <td>${(result._admin) ? 'Sim' : 'Não'}</td>
-                    <td>${Utils.dateFormat(result._register)}</td>
-                    <td>
-                        <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-                        <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-                    </td>
-                `;
+                    user.loadFromJSON(result);
 
-                    this.addEventsTr(tr);
+                    user.save();
+
+                    this.getTr(user, tr);
 
                     this.updateCount();
+
+                    
                     this.formUpdateEl.reset();
                     btn.disabled = false;
 
@@ -90,7 +83,7 @@ class UserController {
             this.getPhoto(this.formEl).then(
                 (content) => {
                     values.photo = content;
-                    this.insert(values);
+                    values.save();
                     this.addLine(values);
                     btn.disabled = false;
                     this.formEl.reset();
@@ -168,18 +161,10 @@ class UserController {
             user.admin);
     }
 
-    getUsersStorage(){
-        let users = [];
-
-        if(localStorage.getItem("users")){
-            users = JSON.parse(localStorage.getItem("users"));
-        }
-
-        return users;
-    }
+    
 
     selectAll(){
-        let users = this.getUsersStorage();
+        let users = User.getUsersStorage();
         users.forEach(dataUser => {
             let user = new User();
             user.loadFromJSON(dataUser);
@@ -199,7 +184,21 @@ class UserController {
     }
 
     addLine(dataUser) {
-        let tr = document.createElement('tr');
+        let tr = this.getTr(dataUser); 
+
+        
+
+        
+
+        this.addEventsTr(tr);
+
+        this.tableEl.appendChild(tr);
+
+        this.updateCount();
+    }
+
+    getTr(dataUser, tr = null){
+        if(tr === null) tr = document.createElement('tr');
 
         tr.dataset.user = JSON.stringify(dataUser);
 
@@ -217,15 +216,17 @@ class UserController {
 
         this.addEventsTr(tr);
 
-        this.tableEl.appendChild(tr);
-
-        this.updateCount();
+        return tr;
     }
 
     addEventsTr(tr) {
 
         tr.querySelector(".btn-delete").addEventListener("click", e => {
             if(confirm("Deseja realmente excluir?")){
+                let user = new User();
+                user.loadFromJSON(JSON.parse(tr.dataset.user));
+                user.remove();
+
                 tr.remove();
                 this.updateCount();
             }
--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 class User {
     constructor(name,gender, birth, country, email, password, photo, admin ){
+        this._id;
         this._name = name;
         this._gender = gender;
         this._birth = birth;
@@ -26,6 +27,69 @@ class User {
         } 
     }
 
+    static getUsersStorage(){
+        let users = [];
+
+        if(localStorage.getItem("users")){
+            users = JSON.parse(localStorage.getItem("users"));
+        }
+
+        return users;
+    }
+
+    getNewID(){
+
+        let usersID = parseInt(localStorage.getItem("usersID"));
+
+        if(!usersID > 0) usersID = 0;
+
+        usersID++;
+
+        localStorage.setItem("usersID", usersID);
+
+        return usersID;
+    }
+
+    remove(){
+        let users = User.getUsersStorage();
+        users.forEach((userData, index) => {
+            if (this._id == userData._id){
+                users.splice(index, 1);
+            }
+        });
+
+        localStorage.setItem("users",JSON.stringify(users));
+
+    }
+
+    save(){
+        let users = User.getUsersStorage();
+
+        let newUser = Object.assign({}, users, this);
+
+        if(this.id > 0){
+            
+            users.map(u=>{
+
+                if (u._id == this.id){
+                    
+                    Object.assign(u, this);
+                }
+                return u;
+            });
+
+        }else { 
+            this._id =  this.getNewID();
+            users.push(this);
+        }
+
+        localStorage.setItem("users",JSON.stringify(users));
+
+        
+    }
+    get id(){
+        return this._id;
+    }
     get register(){
         return this._register;
     }
